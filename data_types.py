52cafeab2d43d6480468f3d6ff0766159417653e
Magzhan Gabdrakhman <gabdrahmanmagzan@gmail.com>
2026-01-27 13:50:19 +0500

    update

---
+++ data_types.py
@@ -0,0 +1,15 @@
+x = 5
+print(type(x))
+
+x = "Hello World"
+print(x)
+x = 20
+print(x)
+
+x = {"apple", "banana", "cherry"}
+
+x = True
+
+x = None
+
+x = 20.5
---
+++ int.py
@@ -0,0 +1,18 @@
+a=5
+b=10
+c=pow(a, b)
+print(c)
+
+print(3)
+print(358)
+print(50000)
+
+print(3 + 3)
+print(2 * 5)
+
+
+
+b = "Hello, World!"
+print(b[2:5])0
+
+print("I am", 35, "years old.")
